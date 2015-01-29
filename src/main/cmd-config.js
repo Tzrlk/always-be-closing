f64fb0a634ae32e69fca85c0e65bbe0f0ab6c02a
pcummuskey <pcummuskey@bravurasolutions.com>
2015-01-29 16:57:14 +1300

    Changed init command to be config and added functional outline.

---
+++ src/main/cmd-config.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+/**
+ * This command creates and/or edits the task management configuration for the current location.
+ * @param {Object} config.
+ */
+function config(config) {
+
+	// TODO: Check to see if a configuration file already exists.
+	// TODO: If exists, update with provided properties
+	// TODO: if not exists, search upward until one is found.
+	// TODO: if not found, prompt for new
+	// TODO: if found, prompt for update, or new
+
+}
+
+module.exports = config;
